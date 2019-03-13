93da99509aec00c024b5eccd635ff5c36a480ac1
Dave <dave.emons997@gmail.com>
2019-03-13 14:53:22 +0100

    Created weather interface with data.

---
+++ app/src/main/java/com/example/forekast/WeatherInterface.java
@@ -0,0 +1,11 @@
+package com.example.forekast;
+
+abstract class WeatherInterface {
+    int temp;
+    int uv_index;
+    int precipitation;
+    int weather_icon;
+    int feels_like;
+    int wind;
+    String city;
+}
