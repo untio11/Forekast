42bf5a8df4e5620a5bd575c8e1f81701cb4375ac
Robin Kneepkens <robin.kneepkens@hotmail.com>
2019-03-13 15:24:53 +0100

    Added clothing types enum + clothing properties interface

---
+++ app/src/main/java/com/example/forekast/ClothingInterface.java
@@ -0,0 +1,5 @@
+package com.example.forekast;
+
+abstract class ClothingInterface extends ClothingProperties {
+
+}
---
+++ app/src/main/java/com/example/forekast/ClothingProperties.java
@@ -0,0 +1,23 @@
+package com.example.forekast;
+
+import android.media.Image;
+
+abstract class ClothingProperties {
+    long ID;
+    String owner;
+    ClothingType type;
+    int comfort;
+    int warmth;
+    int formality;
+    int preference;
+    int[] color;
+    boolean washing_machine;
+    boolean last_washing_state;
+    int washing_time;
+    Image picture;
+
+    @Override
+    public String toString() {
+        return owner + "'s " + type + " [" + ID + "]: (" + warmth + ", " + formality + ", " + comfort + ")";
+    }
+}
