b34e660ac3523f140d3611ba37f5a2fd29fb182d
Barthof <barthof98@gmail.com>
2019-03-15 20:20:33 +0100

    Added fragment for editscreen

--- app/src/main/java/com/example/forekast/Wardrobe.java
+++ app/src/main/java/com/example/forekast/Wardrobe.java
@@ -1,10 +1,13 @@
 package com.example.forekast;
 
+import android.support.v4.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,9 +15,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.Button;
 
-public class Wardrobe extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+import com.example.forekast.dummy.DummyContent;
+
+public class Wardrobe extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, WardrobeFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +44,21 @@ public class Wardrobe extends AppCompatActivity implements NavigationView.OnNavi
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        if(savedInstanceState == null) {
+            Fragment fragment = WardrobeFragment.newInstance(5);
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.wardrobefragment, fragment).commit();
+        }
+
+        Button button = (Button) findViewById(R.id.editbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Fragment fragment = EditScreen.newInstance();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.wardrobefragment, fragment).commit();
+            }
+        });
+
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
@@ -55,7 +76,7 @@ public class Wardrobe extends AppCompatActivity implements NavigationView.OnNavi
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.settings, menu);
+        getMenuInflater().inflate(R.menu.wardrobe, menu);
         return true;
     }
 
@@ -93,5 +114,10 @@ public class Wardrobe extends AppCompatActivity implements NavigationView.OnNavi
 
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
