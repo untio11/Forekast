7916fb9b0357e00d038d7a6a732db63c9ac483f8
Robin Kneepkens <robin.kneepkens@hotmail.com>
2019-03-25 18:46:56 +0100

    Removed useless folder

--- 2IS70/app/src/main/java/com/example/forekast/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.forekast;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Clothing.class}, version =  1)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract ClothingDao clothingDao();
-}
