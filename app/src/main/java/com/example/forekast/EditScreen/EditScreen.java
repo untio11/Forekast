c10c1e2162f884916f89d84ab3be22b452b7ce63
Echidori <38856806+Echidori@users.noreply.github.com>
2019-03-27 10:33:35 +0100

    made edit image, created spinner, made seekBars accessible,  now mostly needs repository/database functionality

--- app/src/main/java/com/example/forekast/EditScreen/EditScreen.java
+++ app/src/main/java/com/example/forekast/EditScreen/EditScreen.java
@@ -1,38 +1,72 @@
 package com.example.forekast.EditScreen;
 
-import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.forekast.R;
 import com.example.forekast.Wardrobe.WardrobeFragment;
 import com.example.forekast.clothing.Clothing;
 
-public class EditScreen extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProviders;
+
+public class EditScreen extends Fragment implements AdapterView.OnItemSelectedListener{
 
     private EditScreenViewModel mViewModel;
 
     public static EditScreen newInstance(Clothing clothing) {
         // Do what you want with clothing here
+
         return new EditScreen();
     }
 
+    String items[] = {"Dress", "Jacket", "Jeans", "Sandals", "Shirt", "Shoes",
+            "Shorts", "Skirt", "Sneakers", "Sweater", "Tanktop", "Trousers", "T-shirt"};
     public static EditScreen newInstance() {
         return new EditScreen();
     }
-
+    ImageView imageView;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.edit_screen_fragment, container, false);
         ImageButton returnbutton = (ImageButton) view.findViewById(R.id.returnbutton);
+
+        //create the camera button, and take into consideration the clothing image
+        Button redoImage = (Button) view.findViewById(R.id.redoImage);
+        imageView = (ImageView) view.findViewById(R.id.imageView10);
+
+        //create clothing type spinner, along with the choice of type, and the adapter it needs with it
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        //implement the different seek bars to regulate warmth, comfort and formality
+        SeekBar seekWarmth = (SeekBar) view.findViewById(R.id.seekWarmth);
+        SeekBar seekComfort = (SeekBar) view.findViewById(R.id.seekComfort);
+        SeekBar seekFormality = (SeekBar) view.findViewById(R.id.seekFormality);
+        //seekFormality: listen to what changes
+        //seekComfort: listen to what changes
+        //seekWarmth: listen to what changes
+
 
         returnbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,7 +76,26 @@ public class EditScreen extends Fragment {
                 transaction.replace(R.id.wardrobefragment, fragment).commit();
             }
         });
+
+        redoImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
         return view;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getActivity(), items[position], Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+// TODO Auto-generated method stub
+
     }
 
     @Override
@@ -52,16 +105,11 @@ public class EditScreen extends Fragment {
         // TODO: Use the ViewModel
     }
 
-    /*
-    //get the spinner from the xml.
-    Spinner dropdown = findViewById(R.id.spinner1);
-    //create a list of items for the spinner.
-    String[] items = new String[]{"", "2", "three"};
-    //create an adapter to describe how the items are displayed, adapters are used in several places in android.
-    //There are multiple variations of this, but this is the basic variant.
-    ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-    //set the spinners adapter to the previously created one.
-    dropdown.setAdapter(adapter);
-    */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+    }
 
 }
