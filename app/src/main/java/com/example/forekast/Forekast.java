6656562f56adac39904d3a36422921090869eb03
Robin Kneepkens <robin.kneepkens@hotmail.com>
2019-04-03 12:16:52 +0200

    Menu item highlighting now works with the back button

--- app/src/main/java/com/example/forekast/Forekast.java
+++ app/src/main/java/com/example/forekast/Forekast.java
@@ -1,9 +1,12 @@
 package com.example.forekast;
 
+
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.SeekBar;
 
@@ -15,19 +18,27 @@ import com.example.forekast.Wardrobe.Wardrobe;
 import com.example.forekast.external_data.Repository;
 import com.example.forekast.external_data.Weather;
 import com.example.forekast.homescreen.HomeScreen;
+import com.google.android.material.internal.NavigationMenuItemView;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 public class Forekast extends AppCompatActivity implements Wardrobe.OnFragmentInteractionListener {
+    private NavigationView navigationView;
+    private int nav_home_id;
+    private int nav_wardrobe_id;
+    private int nav_settings_id;
+
     @Override
     protected void onCreate(Bundle savedState) {
         super.onCreate(savedState);
@@ -56,8 +67,11 @@ public class Forekast extends AppCompatActivity implements Wardrobe.OnFragmentIn
 
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        nav_home_id = R.id.nav_home;
+        nav_settings_id = R.id.nav_settings;
+        nav_wardrobe_id = R.id.nav_wardrobe;
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(item -> {
             // Handle navigation view item clicks here.
             item.setChecked(true);
@@ -65,28 +79,36 @@ public class Forekast extends AppCompatActivity implements Wardrobe.OnFragmentIn
             drawer.closeDrawer(GravityCompat.START);
 
             Fragment fragment = null;
+            String tag = null;
 
             switch (item.getItemId()) {
                 case (R.id.nav_home):
                     // Switch to home screen fragment
                     fragment = HomeScreen.newInstance();
+                    tag = "home";
                     break;
                 case (R.id.nav_wardrobe):
                     fragment = Wardrobe.newInstance();
+                    tag = "wardrobe";
                     // Switch to wardrobe fragment
                     break;
                 case (R.id.nav_settings):
                     fragment = new SettingsFragments();
+                    tag = "settings";
                     break;
             }
 
             if (fragment != null) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.content_area, fragment).addToBackStack(null).commit();
+                FragmentTransaction transaction = Forekast.this.getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.content_area, fragment).addToBackStack(tag).commit();
             }
 
             return true;
         });
+
+        Fragment fragment = HomeScreen.newInstance();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content_area, fragment).addToBackStack("home").commit();
 
         Repository.initDB(getApplicationContext());
     }
@@ -97,6 +119,25 @@ public class Forekast extends AppCompatActivity implements Wardrobe.OnFragmentIn
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
+            int index = getSupportFragmentManager().getBackStackEntryCount() - 2;
+            FragmentManager.BackStackEntry backEntry = getSupportFragmentManager().getBackStackEntryAt(index);
+            String tag = backEntry.getName();
+
+            int menu_index = 0;
+
+            switch (tag) {
+                case ("home"):
+                    menu_index = 0;
+                    break;
+                case ("wardrobe"):
+                    menu_index = 1;
+                    break;
+                case ("settings"):
+                    menu_index = 2;
+                    break;
+            }
+
+            navigationView.getMenu().getItem(menu_index).setChecked(true);
             super.onBackPressed();
         }
     }
